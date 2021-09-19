b7bb8c82781532a1c7b2f7a780ca6dc6efdaf91e
Joshua Gollaher <joshuagollaher@icloud.com>
2021-09-19 12:11:26 -0500

    changed App to function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import {MapContainer, TileLayer, Marker, Popup, useMapEvents} from 'react-leaflet'
-import {Component, useState} from "react";
+import {Component, useEffect, useState} from "react";
 import data from "./attractions.csv"
 import * as d3 from "d3"
 
@@ -8,63 +8,51 @@ import LocationMarker from "./components/LocationMarker";
 import Navbar from "./components/Navbar";
 
 
-class App extends Component {
+const App = () => {
 
-    constructor(props) {
-        super(props);
+    const [state, setState] = useState({
+        data: [],
+        userLocation: [],
+    });
 
-        this.state = {
-            data: [],
-            userLocation: [],
-            map: null
-        };
-
-
-    }
-
-    componentDidMount() {
+    useEffect(() => {
         d3.csv(data).then(function(data) {
-            this.setState({data: data});
-        }.bind(this));
+            setState({...state, data: data});
+        });
+    }, []);
 
+    return (
+        <div>
 
+            <Navbar/>
 
-    }
+            <MapContainer center={[38.20842814139659, -91.16052229681911]}
+                          zoom={12}
+                          scrollWheelZoom={true}
+                          ref={(m) => setState({...state, map: m})}>
 
-    render() {
-        return (
-            <div>
+                <TileLayer
+                    attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
 
-                <Navbar />
+                    url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+                />
 
-                <MapContainer center={[38.20842814139659, -91.16052229681911]}
-                              zoom={12}
-                              scrollWheelZoom={true}
-                              ref={(m) => this.setState({map: m})}>
+                {state.data.map(function (d) {
+                    console.log(d);
+                    return (
+                        <Marker key={d.name + d.latitude} position={[d.latitude, d.longitude]}>
+                            <Popup>
+                                {d.name}
+                            </Popup>
+                        </Marker>
+                    );
+                })}
 
-                    <TileLayer
-                        attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+                <LocationMarker onUserLocation={(loc) => setState({...state, userLocation: loc})}/>
 
-                        url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
-                    />
-
-                    {this.state.data.map(function (d) {
-                        console.log(d);
-                        return (
-                            <Marker key={d.name + d.latitude} position={[d.latitude, d.longitude]}>
-                                <Popup>
-                                    {d.name}
-                                </Popup>
-                            </Marker>
-                        );
-                    })}
-
-                    <LocationMarker onUserLocation={(loc) => this.setState({userLocation: loc})}/>
-
-                </MapContainer>
-            </div>
-        );
-    };
+            </MapContainer>
+        </div>
+    );
 }
 
 export default App;
