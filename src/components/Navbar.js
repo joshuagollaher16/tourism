ba94f9e750cb6d2e187abcffdd3ef9fd847ab3c6
Joshua Gollaher <joshuagollaher@icloud.com>
2021-09-19 22:22:42 -0500

    a

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,66 @@
 import {React, Component, useState} from "react";
 
-import {ToggleButtonGroup, ToggleButton} from "@mui/material";
+import {ToggleButtonGroup, ToggleButton, Container, Rating, Grid} from "@mui/material";
 
 export default function Navbar() {
 
-    const [alignment, setAlignment] = useState('rating');
+    const [alignment, setAlignment] = useState('');
 
     const handleChange = (event, newAlignment) => {
         setAlignment(newAlignment);
     };
 
     return (
-        <ToggleButtonGroup
-            color="primary"
-            value={alignment}
-            exclusive
-            onChange={handleChange}
-        >
-            <ToggleButton className="full-height" value="rating">Rating</ToggleButton>
-            <ToggleButton className="full-height" value="nearme">Near Me</ToggleButton>
-            <ToggleButton className="full-height" value="price">Price</ToggleButton>
-            <ToggleButton className="full-height" value="seasonal">Seasonal</ToggleButton>
-            <ToggleButton className="full-height" value="type">Type</ToggleButton>
-        </ToggleButtonGroup>
+
+        <Grid container direction="row" alignItems="center" spacing={2} className="navbar">
+            <Grid item>
+                <ToggleButtonGroup
+                    color="primary"
+                    value={alignment}
+                    exclusive
+                    onChange={handleChange}>
+                    <ToggleButton className="full-height" value="rating">Rating</ToggleButton>
+                    <ToggleButton className="full-height" value="nearme">Near Me</ToggleButton>
+                    <ToggleButton className="full-height" value="price">Price</ToggleButton>
+                    <ToggleButton className="full-height" value="seasonal">Seasonal</ToggleButton>
+                    <ToggleButton className="full-height" value="type">Type</ToggleButton>
+                </ToggleButtonGroup>
+            </Grid>
+
+
+            <Grid item>
+                {alignment === "rating" &&
+                <Rating size="large">
+
+                </Rating>
+                }
+
+                {alignment === "nearme" &&
+                <Rating size="large">
+
+                </Rating>
+                }
+
+                {alignment === "price" &&
+                <Rating size="large">
+
+                </Rating>
+                }
+
+                {alignment === "seasonal" &&
+                <Rating size="large">
+
+                </Rating>
+                }
+
+                {alignment === "type" &&
+                <Rating size="large">
+
+                </Rating>
+                }
+
+            </Grid>
+
+        </Grid>
     );
 }
